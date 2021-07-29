8c4d3bedb2c0adff061d385b4601ed20f624ef82
Devon <devonross50@yahoo.com>
2021-07-28 19:55:15 -0500

    updated server/server.js

---
+++ server/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers }=require('./schemas');
+const {authMiddleware}=require('./utils/auth');
+const db=require('./config/connection');
+const path = require('path')
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+let server = null;
+async function startServer() {
+    server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        context: authMiddleware
+    })
+    await server.start();
+    server.applyMiddleware({ app })
+}
+startServer();
+
+app.use(express.static(path.join(__dirname, '../client/build')))
+
+app.get('*', (req,res)=> {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'))
+})
+
+db.once('open', ()=> {
+    app.listen(PORT, ()=> {
+        console.log(`Now listening on localhost:${PORT}`)
+        console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
+    })
+})
