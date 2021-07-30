037891f6088036f95801164dc1a42e70a707d917
Devon <devonross50@yahoo.com>
2021-07-29 20:58:42 -0500

    updated bracket model

--- server/models/Bracket.js
+++ server/models/Bracket.js
@@ -6,9 +6,17 @@ const bracketSchema = new Schema({
             type: String,
         },
     ],
+        creator: {
+            type: String,
+            require: true,
+        },
         createdAt: {
             type: Date,
             default: Date.now
+        },
+        username: {
+            type: String,
+            required: true,
         },
         seed:  {
             type: Integer,
@@ -25,8 +33,19 @@ const bracketSchema = new Schema({
         round: {
             type: Integer,
             required: true,
+        }, 
+        group: {
+            type: Integer,
+            required: true,
         }
     
-});
+,
+}, 
+{
+    toJSON: {
+        virtuals: true,
+    }
+}
+);
 
 module.exports = bracketSchema;
