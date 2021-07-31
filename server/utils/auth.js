18d8842b4735c129f6d784311202a7588ac02052
Adam Olson <adamlsn@gmail.com>
2021-07-31 11:48:23 -0500

    commit before pull

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,15 +1,5 @@
 const jwt = require('jsonwebtoken');
 
-<<<<<<< HEAD
-const secret = 'secretsercetIvegotasecret';
-const expiration = '96h';
-
-module.exports = {
-    signToken: function ({ username, email, _id}) {
-        const payload = { username, email, _id};
-
-        return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-=======
 const secret = 'mysecrettt';
 const expiration = '96h';
 
@@ -38,6 +28,5 @@ module.exports = {
             console.log('Wrong Token')
         }
         return req;
->>>>>>> origin/feature/user
     }
 }
