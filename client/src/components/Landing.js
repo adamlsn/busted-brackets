63392fbd78d13dddf5c776e0212963be881c3366
Your Name <you@example.com>
2021-08-01 20:09:41 -0500

    messing with styling and page layout

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -2,23 +2,25 @@ import { Link } from "react-router-dom";
 
 function Landing() {
   return (
-    <div
-      className="hero"
-      style={{
-        textAlign: "center",
-      }}
-    >
-      <h1 className="heroTitle">BUSTED BRACKETS</h1>
-      <div className="button-hero">
-        <Link to="/create">Create Profile</Link>
+    <section>
+      <div
+        className="hero"
+        style={{
+          textAlign: "center",
+        }}
+      >
+        <h1 className="heroTitle">BUSTED BRACKETS</h1>
+        <div className="button-hero">
+          <Link to="/create">Create Profile</Link>
+        </div>
+        <div className="button-hero">
+          <Link to="/login">Login</Link>
+        </div>
+        <div className="button-hero">
+          <Link to="/about">About</Link>
+        </div>
       </div>
-      <div className="button-hero">
-        <Link to="/login">Login</Link>
-      </div>
-      <div className="button-hero">
-        <Link to="/about">About</Link>
-      </div>
-    </div>
+    </section>
   );
 }
 
