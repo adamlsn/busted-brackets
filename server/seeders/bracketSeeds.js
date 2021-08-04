b1ab22648253544c14cef0eb67d0287f44566b0f
Adam Olson <adamlsn@gmail.com>
2021-08-03 19:24:53 -0500

    hardcoding data into seeders

--- server/seeders/bracketSeeds.js
+++ server/seeders/bracketSeeds.js
@@ -8,7 +8,40 @@ db.once('open', async () => {
 
   // create user data
   const bracketData = [];
-  
+  const teamsArr = [
+    "Arizona Cardinals",
+    "Atlanta Falcons",
+    "Buffalo Bills",
+    "Baltimore Ravens",
+    "Carolina Panthers",
+    "Cincinnati Bengals",
+    "Cleveland Browns",
+    "Chicago Bears",
+    "Dallas Cowboys",
+    "Denver Broncos",
+    "Detroit Lions",
+    "Green Bay Packers",
+    "Houston Texans",
+    "Indianapolis Colts",
+    "Kansas City Chiefs",
+    "Los Angeles Chargers",
+    "Los Angeles Rams",
+    "Jacksonville Jaguars",
+    "Miami Dolphins",
+    "Minnesota Vikings",
+    "New England Patriots",
+    "New Orleans Saints",
+    "New York Giants",
+    "New York Jets",
+    "Oakland Raiders",
+    "Philadelphia Eagles",
+    "San Francisco 49ers",
+    "Seattle Seahawks",
+    "Pittsburgh Steelers",
+    "Tampa Bay Buccaneers",
+    "Tennessee Titans",
+    "Washington Redskins"
+  ]
 
   for (let i = 0; i < 5; i += 1) {
     const group = [];
@@ -19,7 +52,7 @@ db.once('open', async () => {
     const createdAt = faker.date.recent();
     const seed = 1;
     const id = 2;
-    const teams = faker.company.companyName();
+    const teams = teamsArr;
     const round = 3;
     const teamOne = faker.commerce.productName();
     const teamTwo = faker.commerce.productName();
