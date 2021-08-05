b6494ecaa74c02c129b488fb2e157363180c8789
Adam Olson <adamlsn@gmail.com>
2021-08-04 22:16:23 -0500

    editing server file

--- server/server.js
+++ server/server.js
@@ -22,13 +22,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Serve up static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+app.get('*', function (req, res) {
+  const index = path.join(__dirname, 'client','build', 'index.html');
+  res.sendFile(index);
 });
+
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
