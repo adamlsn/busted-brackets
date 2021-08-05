a811ed252a32264674a6900ce2964d2c98ec8371
csr-ph <lucaw10@hotmail.com>
2021-08-04 19:39:25 -0500

    css tests

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const  Footer = () => {
     return (
-        <footer className="w-100 mt-auto bg-secondary p-4">
-            <div className="container">by Adam,Devon,Kaan,Luca,Mitchell</div>
+        <footer className="w-100 mt-auto bg-secondary p-4 footer-container">
+            <div className="container" style={{color: "yellow"}}>by Adam,Devon,Kaan,Luca,Mitchell</div>
         </footer>
     );
 }
